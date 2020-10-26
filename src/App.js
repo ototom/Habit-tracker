cf7441a570ff22d777eb8b06e0ce03ef95bde1d2
ototom <to.krawczyk@gmail.com>
2020-10-26 11:14:53 +0100

    Add responsive sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Sidebar from './components/Sidebar/Sidebar';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+    const closeSidebarHandler = useCallback(() => setSidebarOpen(false), []);
+    const openSidebarHandler = () => setSidebarOpen(true);
+
     return (
-        <Switch>
-            <Route exact path='/'>
-                Content
-            </Route>
-        </Switch>
+        <>
+            <Sidebar
+                closeSidebarHandler={closeSidebarHandler}
+                isSidebarOpen={isSidebarOpen}
+            />
+            <main className='container'>
+                <Switch>
+                    <Route exact path='/'>
+                        <header>
+                            {!isSidebarOpen && (
+                                <button
+                                    className='menu-btn'
+                                    onClick={openSidebarHandler}
+                                >
+                                    <i className='fas fa-bars'></i>
+                                </button>
+                            )}
+                            <h1>Page title</h1>
+                        </header>
+                        Content
+                    </Route>
+                </Switch>
+            </main>
+        </>
     );
 }
 
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import Backdrop from '../shared/Backdrop/Backdrop';
+import './Sidebar.scss';
+
+const Sidebar = ({ isSidebarOpen, closeSidebarHandler }) => {
+    const history = useHistory();
+
+    useEffect(() => {
+        history.listen(closeSidebarHandler);
+    }, [history, closeSidebarHandler]);
+
+    return (
+        <>
+            <aside
+                className={`sidebar ${isSidebarOpen ? 'sidebar--is-open' : ''}`}
+            >
+                <section className='sidebar__welcome-msg'>
+                    <img
+                        className='sidebar__avatar circle-img'
+                        src='https://via.placeholder.com/40'
+                        alt=''
+                    />
+                    Hello, Tom!
+                </section>
+                <nav className='sidebar__section'>
+                    <h3>Account</h3>
+                    <ul>
+                        <li>
+                            <NavLink to='/edit'>
+                                <i className='fas fa-user'></i>Edit profile
+                            </NavLink>
+                        </li>
+                        <li>
+                            <button>
+                                <i className='fas fa-sign-out-alt'></i>Logout
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+                <nav className='sidebar__section'>
+                    <h3>Main menu</h3>
+                    <ul>
+                        <li>
+                            <NavLink exact to='/'>
+                                <i className='fas fa-home'></i>Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/sum'>
+                                <i className='fas fa-chart-bar'></i>Summary
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <nav className='sidebar__section'>
+                    <h3>Habits</h3>
+                    <ul>
+                        <li>
+                            <NavLink to='/hab1'>
+                                <i className='fas fa-dot-circle'></i>Habit 1
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/hab2'>
+                                <i className='fas fa-dot-circle'></i>Habit 2
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/hab3'>
+                                <i className='fas fa-dot-circle'></i>Habit 3
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+            {isSidebarOpen && <Backdrop onClick={closeSidebarHandler} />}
+        </>
+    );
+};
+
+export default Sidebar;
